ad6beb53017d55fd5ca53a17b2efe69a74d13a4a
andreamazza84 <andrea.mazza84@gmail.com>
2020-11-18 15:37:19 +0100

    Commit iniziale

---
+++ main.js
@@ -0,0 +1,4 @@
+//Chiedi all'utente il cognome
+//Inseriscilo in un array con altri cognomi
+//Stampa la lista ordinata alfabeticamente
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova.
